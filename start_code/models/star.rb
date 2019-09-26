556313080ccf7ed19fb438964c8e8324022a7d39
Iona Grant <ionagrant92@gmail.com>
2019-09-26 15:56:19 +0100

    got update to work

--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -27,6 +27,12 @@ class Star
     @id = star['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE stars SET (first_name, last_name) = ($1, $2) WHERE id = $3"
+    values = [@first_name, @last_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all
     sql = "SELECT * FROM castings"
     castings = SqlRunner.run( sql )
