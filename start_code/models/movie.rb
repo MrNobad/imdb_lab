cda21bfb84e5cb387cea252dd3144e88e73aea6f
Iona Grant <ionagrant92@gmail.com>
2019-09-26 16:00:10 +0100

    got movies to update

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -28,6 +28,12 @@ class Movie
       @id = movie['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE movies SET (title, genre) = ($1, $2) WHERE id = $3"
+    values = [@title, @genre, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all
     sql = "SELECT * FROM movies"
     movies = SqlRunner.run( sql )
